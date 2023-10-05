70e9c1ddfa772224db4bcb023c2cf05a6c3e55d1
Anik85 <anikbaruaanik85@gmail.com>
2023-10-05 13:24:25 +0600

    new update

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,9 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use function PHPUnit\Framework\returnSelf;
+
 class Product extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    // public function category(){
+    //     return $this->belongsTo();
+    // }
 }
